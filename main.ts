b128d630fb98b54080dfe348a66a4b22e09c444b
tonyoryo <131649057+tonyoryo@users.noreply.github.com>
2023-06-12 13:54:14 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    cuteBot.forward()
+})
+input.onButtonPressed(Button.B, function () {
+    cuteBot.backforward()
+})
+basic.showIcon(IconNames.Heart)
